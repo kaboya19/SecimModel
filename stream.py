e0f351f63eb623cdfcb43d4b1496b08e42e85e70
Bora Kaya <kayabo19@itu.edu.tr>
2025-03-04 01:31:35 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -135,10 +135,30 @@ if page=="Ana Sayfa":
         }
 
         df = pd.DataFrame(data)
-        
-        st.image("parlemento.png", use_column_width=True)
+
+        Party = namedtuple('Party', ['name', 'color', 'size'])
+        parties = [Party(row['PARTIDO'], row['COLOR'], row['SEATS']) for _, row in df.iterrows()]
+        NUM_DEPUTIES = sum(p.size for p in parties)
+
+        party_seats = [f"**{party.name}: {party.size}**" for party in parties]
+
+        # Yazıları yan yana yazdırmak için markdown kullanma
+        st.markdown(" | ".join(party_seats))
+        st.image("parlemento_zpiyip.png")
     elif scenario== "İYİ-ZP ittifak yapıyor":
-        
+        mv=pd.read_csv("MV_Şub 2025_iyizp.csv",index_col=0)
+        mv=mv.sum()
+        mv=pd.DataFrame(mv)
+
+        # Parlamento Verisi (Örnek)
+        data = {
+            "PARTIDO": list(mv.index.values),
+            "COLOR": [ '#FFCC00','#FF5733', '#0000FF', '#800080', '#6600CC','#17e8e8','#9d0404'],
+            "NAME": list(mv.index.values),
+            "SEATS": list(mv.values.reshape(1,-1)[0]),
+        }
+
+        df = pd.DataFrame(data)
 
         import streamlit as st
         import pandas as pd
@@ -146,8 +166,92 @@ if page=="Ana Sayfa":
         import matplotlib.pyplot as plt
         from collections import namedtuple, defaultdict
 
-    
-            
+        
+        # Parti ve sandalye sayıları için namedtuple tanımla
+        Party = namedtuple('Party', ['name', 'color', 'size'])
+        parties = [Party(row['PARTIDO'], row['COLOR'], row['SEATS']) for _, row in df.iterrows()]
+        NUM_DEPUTIES = sum(p.size for p in parties)
+
+        # Fonksiyonlar
+        def calculate_radius(num_rows, initial_radius, radius_increment):
+            """Calculate the radius for each row."""
+            return [initial_radius + i * radius_increment for i in range(num_rows)]
+
+        def calculate_arc_lengths(radius):
+            """Calculate the arc lengths for each radius."""
+            return [r * np.pi for r in radius]
+
+        def calculate_deputies_per_row(num_deputies, arc_lengths, total_arc_length):
+            """Calculate the number of deputies per row."""
+            deputies_per_row = [int(num_deputies * (arc_length / total_arc_length)) for arc_length in arc_lengths]
+
+            # Distribute the remaining deputies
+            diff = num_deputies - sum(deputies_per_row)
+            deputies_per_row[-1] += diff
+            return deputies_per_row
+
+        def generate_points(num_rows, radii, deputies_per_row):
+            """Generate the points for each deputy."""
+            points = []
+            for row in range(num_rows):
+                radius = radii[row]
+                num_deputies_row = deputies_per_row[row]
+                angles = np.linspace(0, np.pi, num_deputies_row)  # Spread deputies across the semicircle
+                x = radius * np.cos(angles)
+                y = radius * np.sin(angles)
+                for i in range(num_deputies_row):
+                    points.append((radius, angles[i], x[i], y[i]))
+            return sorted(points, key=lambda x: x[1], reverse=True)
+
+        def main(num_rows, initial_radius, radius_increment):
+            """Main function to generate deputies' positions."""
+            radius = calculate_radius(num_rows, initial_radius, radius_increment)
+            arc_lengths = calculate_arc_lengths(radius)
+            total_arc_length = sum(arc_lengths)
+            deputies_per_row = calculate_deputies_per_row(NUM_DEPUTIES, arc_lengths, total_arc_length)
+            points = generate_points(num_rows, radius, deputies_per_row)
+
+            Deputy = namedtuple('Deputy', ['x', 'y', 'radius', 'angle'])
+            return [Deputy(x, y, radius, angle) for (radius, angle, x, y) in points]
+
+        def plot_deputies(deputies, parties, POINT_SIZE):
+            """Plot the deputies on a chart."""
+            deputies_by_party = defaultdict(list)
+
+            current_index = 0
+
+            for party in parties:
+                party_deputies = deputies[current_index:current_index + party.size]
+                deputies_by_party[party.name].extend(party_deputies)
+                current_index += party.size
+
+            deputies_by_party = dict(deputies_by_party)
+
+            fig, ax = plt.subplots(figsize=(12, 6))
+
+            for party in parties:
+                color = party.color
+                party_deputies = deputies_by_party[party.name]
+
+                label = party.name
+
+                for deputy in party_deputies:
+                    if color == 'unassigned':
+                        ax.scatter(deputy.x, deputy.y, s=POINT_SIZE, facecolors='none', edgecolors='grey', linewidth=0.75, label=label)
+                    else:
+                        ax.scatter(deputy.x, deputy.y, s=POINT_SIZE, alpha=1, color=color, label=label)
+                    label = ""
+
+            ax.set_aspect('equal')
+            ax.axis('off')
+
+            ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0), ncol=6, frameon=False)
+
+        # Constants
+        NUM_ROWS = 12
+        INITIAL_RADIUS = 30
+        RADIUS_INCREMENT = 5
+        POINT_SIZE = 100
 
         # Streamlit Başlık
         import warnings
@@ -155,11 +259,21 @@ if page=="Ana Sayfa":
         import logging
         import streamlit as st
 
-        
-        st.image("parlemento_zpiyip.png")
+        # Streamlit log seviyelerini ayarlamak için logging modülünü kullanıyoruz
+        logging.basicConfig(level=logging.ERROR)
 
-        # Parti isimleri ve sandalye sayıları
-        
+        deputies = main(NUM_ROWS, INITIAL_RADIUS, RADIUS_INCREMENT)
+
+
+        party_seats = [f"**{party.name}: {party.size}**" for party in parties]
+
+        # Yazıları yan yana yazdırmak için markdown kullanma
+        st.markdown(" | ".join(party_seats))
+        st.image("parlemento_zpiyip.png")
+ 
+
+    # Parti isimleri ve sandalye sayıları
+    
 
 
     
